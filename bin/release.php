5c0ff9e08cb6e764e4ae85c3e3711a9548fdb7de
Sven Vetter <sv@svenvetter.com>
2021-07-03 19:42:34 +0200

    remove satis from release script

--- bin/release.php
+++ bin/release.php
@@ -15,5 +15,4 @@ $res = shell_exec('git push');
 $res = shell_exec('git tag -a ' . $version . ' -m "' . $message . '"');
 $res = shell_exec('git push origin ' . $version);
 
-$res = shell_exec('ssh svenvett@svenvett.myhostpoint.ch  "cd /home/svenvett/www/satis; bin/satis build satis.json"');
 ?>
