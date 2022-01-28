d8add4bdaf478df8458c407b6e909bfa7be3d11e
Sven Vetter <git.09o@svenvetter.com>
2022-01-28 22:18:06 +0100

    alert.js ready for stimulus 3, fixed unit test error

--- bin/release.php
+++ bin/release.php
@@ -1,8 +1,8 @@
 #!/usr/bin/env php
 <?php
 
-$version = "v2.0.0";
-$message = "added initial compatibility to stimulus 3";
+$version = "v2.0.1";
+$message = "alert.js ready for stimulus 3, fixed unit test error";
 
 file_put_contents("CHANGELOG.md", "\n\n## Version " . $version, FILE_APPEND);
 file_put_contents("CHANGELOG.md", "\n*" . date("r") . "*", FILE_APPEND);
