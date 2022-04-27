047c88b57e04742497bbb79019592b22a50b543d
Sven Vetter <git.09o@svenvetter.com>
2022-04-27 22:24:27 +0200

    commited by git_all.php on 2022-04-27 20:24:27

--- bin/release.php
+++ bin/release.php
@@ -4,6 +4,13 @@
 $version = "v2.1.0";
 $message = "ready for symfony 6.0";
 
+echo("Running phpstan:\n");
+system("composer run-script phpstan", $res);
+if ($res>0) {
+  echo("\nError during execution phpstan. Releasing cannceled.\n");
+  return 1;
+}
+
 file_put_contents("CHANGELOG.md", "\n\n## Version " . $version, FILE_APPEND);
 file_put_contents("CHANGELOG.md", "\n*" . date("r") . "*", FILE_APPEND);
 file_put_contents("CHANGELOG.md", "\n- " . $message . "\n", FILE_APPEND);
