7406555c713fa2c5c4a17f822dd6f080e64cd924
Sven Vetter <git.09o@svenvetter.com>
2024-06-16 23:19:55 +0200

    commited by git_all.php on 2024-06-16 21:19:55

---
+++ src/Twig/Components/Table.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Svc\UtilBundle\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('SvcUtil-Table', '@SvcUtil/components/Table.html.twig')]
+final class Table
+{
+  public bool $hasHeader = true;
+  public bool $isResponsive = true;
+  public bool $isSmall = false;
+  public bool $isStriped = false;
+  public bool $isBordered = false;
+
+  public function getSmall(): ?string {
+    return $this->isSmall ? 'table-sm ' : null;
+  }
+
+  public function getStriped(): ?string {
+    return $this->isStriped ? 'table-striped ' : null;
+  }
+
+  public function getBordered(): ?string {
+    return $this->isBordered ? 'table-bordered ' : null;
+  }
+}
